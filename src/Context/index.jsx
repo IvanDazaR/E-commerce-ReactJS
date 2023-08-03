692bb3da2d7d864cc3faeccd280137de40e5edfe
ivandaza <talivan0717@hotmail.com>
2023-08-03 15:29:51 -0500

    Layout the ProductDetail

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -4,11 +4,17 @@ export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
     const [count, setCount] = useState(0);
+    const [isProductOpen, setIsProductOpen] = useState(false);
 
+    const openProductDetail = () => setIsProductOpen(true)
+    const closeProductDetail = () => setIsProductOpen(false)
     return (
         <ShoppingCartContext.Provider value={{
             count,
+            isProductOpen,
             setCount,
+            openProductDetail,
+            closeProductDetail,
         }}>
             { children }
         </ShoppingCartContext.Provider>
