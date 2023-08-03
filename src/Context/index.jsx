d9d41570c1fc4bb6601367f761c6d820bc72d9d5
ivandaza <talivan0717@hotmail.com>
2023-08-03 16:04:55 -0500

    Show products in ProductDetail component

--- src/Context/index.jsx
+++ src/Context/index.jsx
@@ -3,18 +3,30 @@ import { createContext, useState } from "react";
 export const ShoppingCartContext = createContext();
 
 export const ShoppingCartProvider = ({ children }) => {
+    //Shopping Cart - Increment quantity
     const [count, setCount] = useState(0);
-    const [isProductOpen, setIsProductOpen] = useState(false);
 
+    //Product Detail - Open/Close
+    const [isProductOpen, setIsProductOpen] = useState(false);
     const openProductDetail = () => setIsProductOpen(true)
     const closeProductDetail = () => setIsProductOpen(false)
+
+    // Product Detail - Show product
+    const [productToShow, setProductToShow] = useState({
+        title: '',
+        price: '',
+        description: '',
+        images: [],
+    });
     return (
         <ShoppingCartContext.Provider value={{
             count,
             isProductOpen,
+            productToShow,
             setCount,
             openProductDetail,
             closeProductDetail,
+            setProductToShow,
         }}>
             { children }
         </ShoppingCartContext.Provider>
